e2777d3448bfc317edfdbbc2f3b0cd1d82adf673
Michael <michael_makam18@hotmail.com>
2022-02-02 14:35:34 +0000

    created ActorEntityAssembler to simplify link creation

---
+++ src/main/java/com/sparta/mm/springrestapi/assembler/ActorEntityAssembler.java
@@ -0,0 +1,23 @@
+package com.sparta.mm.springrestapi.assembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+
+import com.sparta.mm.springrestapi.controller.ActorController;
+import com.sparta.mm.springrestapi.entities.ActorEntity;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+public class ActorEntityAssembler implements RepresentationModelAssembler<ActorEntity, EntityModel<ActorEntity>> {
+
+    @Override
+    public EntityModel<ActorEntity> toModel(ActorEntity entity) {
+        return EntityModel.of(entity,
+                linkTo(methodOn(ActorController.class).findActorById(entity.getActorId())).withSelfRel(),
+                linkTo(methodOn(ActorController.class).getActorsByName(entity.getFirstName().toLowerCase(), entity.getActorId())).withRel("actors"));
+    }
+}
