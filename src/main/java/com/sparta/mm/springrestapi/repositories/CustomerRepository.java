eb4b602798b8468c7486816154a89ffcea8d5746
Michael <michael_makam18@hotmail.com>
2022-02-02 15:12:10 +0000

    edited CustomerRepository - added @Repository tag to interface

--- src/main/java/com/sparta/mm/springrestapi/repositories/CustomerRepository.java
+++ src/main/java/com/sparta/mm/springrestapi/repositories/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.sparta.mm.springrestapi.repositories;
 
 import com.sparta.mm.springrestapi.entities.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity,Integer> {
 }
