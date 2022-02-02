108c2109d1c6861a3d7a1d7098c4082ee29a11be
Michael <michael_makam18@hotmail.com>
2022-02-02 16:29:19 +0000

    edited ActorEntityAssembler - small layout change

--- src/main/java/com/sparta/mm/springrestapi/assembler/ActorEntityAssembler.java
+++ src/main/java/com/sparta/mm/springrestapi/assembler/ActorEntityAssembler.java
@@ -9,8 +9,6 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
-import java.util.Locale;
-
 @Component
 public class ActorEntityAssembler implements RepresentationModelAssembler<ActorEntity, EntityModel<ActorEntity>> {
 
