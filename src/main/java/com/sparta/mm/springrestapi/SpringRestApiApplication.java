99c6e7de02af7f143036fa38bfb92aea1aa45929
Michael <michael_makam18@hotmail.com>
2022-02-03 12:00:19 +0000

    edited SpringRestApiApplication

--- src/main/java/com/sparta/mm/springrestapi/SpringRestApiApplication.java
+++ src/main/java/com/sparta/mm/springrestapi/SpringRestApiApplication.java
@@ -1,7 +1,12 @@
 package com.sparta.mm.springrestapi;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringRestApiApplication {
@@ -9,5 +14,18 @@ public class SpringRestApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringRestApiApplication.class, args);
     }
+
+    @Bean
+    public OpenAPI springSakilaAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Sakila API")
+                        .description("Sakila Database used for film store")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringRESTAPI")
+                        .url("https://github.com/M1M8/SpringRESTAPI"));
+    }
+
 
 }
